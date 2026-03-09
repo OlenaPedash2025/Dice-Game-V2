0320dcff5946f192951c8bc330089aa0c3fe6b84
Olena Pedash <olena.pedash@edu.dualis-institut.de>
2026-03-09 10:50:42 +0100

    feat: added statistics and amount of rolls

--- p2_1_1.py
+++ p2_1_1.py
@@ -5,16 +5,23 @@ class Dice_spiel:
     def __init__(self, min_wurf=1, max_wurf=6):
         self.min_wurf = min_wurf
         self.max_wurf = max_wurf
+        self.amount_of_roll = 0
+        self.statistics = {i: 0 for i in range(self.min_wurf, self.max_wurf + 1)}
 
     def roll_dice(self):
-        return random.randint(self.min_wurf, self.max_wurf)
+        number = random.randint(self.min_wurf, self.max_wurf)
+        self.statistics[number] += 1
+        self.amount_of_roll += 1
+        return number
 
     def play_game(self):
         print("Willkommen zum Würfelspiel!")
         while True:
             input("Drücke Enter, um zu würfeln...")
             wurf = self.roll_dice()
-            print(f"Du hast eine {wurf} geworfen!")
+            print(
+                f"Du hast eine {wurf} geworfen! \nDu hast {self.amount_of_roll} Würfe gemacht. \nDie Statistik der Würfe: {self.statistics}"
+            )
             if wurf == 6:
                 print("Glückwunsch! Du hast gewonnen!")
                 break
